6b4acc7b1e8072800c51204e9efd0297d6306870
github.com-aparajita31pandey <aparajita31pandey@gamil.com>
2025-05-09 11:04:16 +0530

    amke working test case

--- wal_test.go
+++ wal_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"reflect"
 	"testing"
 )
 
@@ -22,6 +24,7 @@ type Record struct {
 
 func TestCreate(t *testing.T) {
 	wal, err := Create(true, 1024, 10, "test")
+	defer os.RemoveAll("test")
 	fmt.Println("======wal is ==============", wal)
 	entries := []Record{
 		{Key: "key1", Value: []byte("value1"), Op: InsertOperation},
@@ -33,6 +36,21 @@ func TestCreate(t *testing.T) {
 		assert.NoError(t, err, "Failed to marshal entry")
 		assert.NoError(t, wal.write(marshaledEntry, false), "Failed to write entry")
 	}
+
+	recoveredEntries, err := wal.readAll()
+	fmt.Println("++++++++++++++length is ++++++++++++", len(recoveredEntries))
+	for entryIndex, entry := range recoveredEntries {
+		unMarshalledEntry := Record{}
+		assert.NoError(t, json.Unmarshal(entry.Data, &unMarshalledEntry), "Failed to unmarshal entry")
+		fmt.Println("========each read entry=======", unMarshalledEntry.Key)
+
+		// Can't use deep equal because of the sequence number
+		assert.Equal(t, entries[entryIndex].Key, unMarshalledEntry.Key, "Recovered entry does not match written entry (Key)")
+		assert.Equal(t, entries[entryIndex].Op, unMarshalledEntry.Op, "Recovered entry does not match written entry (Op)")
+		assert.True(t, reflect.DeepEqual(entries[entryIndex].Value, unMarshalledEntry.Value), "Recovered entry does not match written entry (Value)")
+	}
+	assert.NoError(t, err, "Failed to recover entries")
+
 	fmt.Println("===============", err)
 	fmt.Println("++++++++++++++++++++", wal)
 }
